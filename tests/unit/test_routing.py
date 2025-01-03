bc470eb8cc0093e0f0f75d495fff9d9e57227441
Michał Ligus <maveilthain@gmail.com>
2025-01-03 20:49:58 +0100

    #2 feat(sceleton): added and modified some tests for route object

--- tests/unit/test_routing.py
+++ tests/unit/test_routing.py
@@ -1,8 +1,11 @@
+import re
+
 from hypothesis import given
 from hypothesis.strategies import (booleans, floats, from_regex, integers,
                                    lists, one_of, sets, text, tuples)
 from pytest import mark, raises
 from sanic.constants import HTTP_METHODS
+from typing_extensions import Callable
 
 from sanic_adapters.routing import Route
 
@@ -74,11 +77,12 @@ class TestRoute:
         with raises(ValueError):
             Route(name="x", path=path)
 
-    @given(path=from_regex("/[A-Za-z0-9].*"))
+    @given(path=from_regex(r"^/[A-Za-z0-9/]+", fullmatch=True))
     def test_path_passed_correct_type_to_route_when_is_string_than_route_returns_string(self, path: str | int):
         route = Route(name="x", path=path)
+        print(f"[ DEBUG ] {path}")
 
-        assert route.path == path and isinstance(route.path, str), "Path must be string"
+        assert route.path == path and isinstance(route.path, str) and re.match("/[A-Za-z0-9]*", route.path), "Path must be valid string"
 
     def test_when_only_required_fields_are_passed_then_method_field_are_not_set_by_default(self):
         route = Route(name="x", path="/test")
@@ -117,3 +121,33 @@ class TestRoute:
     @mark.parametrize("method", HTTP_METHODS)
     def test_when_correct_method_passed_then_route_accept(self, method: str):
         Route(name="x", path="/test", method=method)
+
+    def test_route_create_when_function_not_passed_then_route_function_is_none(self):
+        route = Route(name="x", path="/test", method="GET")
+        assert not route.function, f"Function expected as None, but got '{type(route.function)}'"
+
+    def test_when_correct_function_passed_then_route_function_is_callable(self):
+        route = Route(name="x", path="/test", method="GET", function=lambda x: x)
+        assert isinstance(route.function, Callable), f"Function expected as callable, but got '{type(route.function)}'"
+
+    def test_route_create_when_class_name_not_passed_then_route_class_name_is_none(self):
+        route = Route(name="x", path="/test", method="GET")
+        assert not route.class_name, f"Function expected as None, but got '{type(route.class_name)}'"
+
+    def test_when_correct_class_name_passed_then_route_class_name_is_set(self):
+        route = Route(name="x", path="/test", method="GET", function=lambda x: x, class_name="string")
+        message = f"Class name expected as correct string, but got '{type(route.class_name)}'"
+        assert route.class_name and isinstance(route.class_name, str), message
+
+    def test_given_created_route_when_route_is_executed_then_function_has_new_attributes(self):
+        # Given
+        route = Route(name="x", path="/test", method="GET", function=lambda x: x, class_name="string")
+
+        # When
+        route()
+
+        # Then
+        assert route.function.name == "x", "Expected route.function has name, but got exception"
+        assert route.function.path == "/test", "Expected route.function has path, but got exception"
+        assert route.function.method == "GET", "Expected route.function has method, but got exception"
+        assert route.function.class_name == "string", "Expected route.function has class_name, but got exception"
