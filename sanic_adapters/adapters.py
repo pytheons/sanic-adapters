349b7a1c5f4ff7110b2b8fdf2fbf21e24df93ca4
Michał Ligus <maveilthain@gmail.com>
2025-01-12 00:14:59 +0100

    #10 feat(sanic_adapters): added ioc container wrapper from dependency injector and for sanic dependencies

--- sanic_adapters/adapters.py
+++ sanic_adapters/adapters.py
@@ -3,13 +3,27 @@ from importlib import import_module
 from importlib.metadata import PackageNotFoundError
 from importlib.util import find_spec
 from pathlib import Path
-from typing import List
+from typing import (
+    List,
+    Tuple,
+)
+
+from dependency_injector import providers
+from dependency_injector.containers import DynamicContainer
+from dependency_injector.providers import Configuration
+from dependency_injector.wiring import provided
+from sanic import Sanic
 
 from sanic_adapters.resources import RoutePart
 from sanic_adapters.routing import Routing
 
 
 class RESTFramework:
+    sanic: Sanic
+
+    def __init__(self, sanic: Sanic, package: str):
+        self.sanic = sanic
+        self.sanic.blueprint(self.autodiscover(package))
 
     @classmethod
     def autodiscover(cls, package: str) -> List[RoutePart]:
@@ -43,3 +57,30 @@ class RESTFramework:
                     modules |= cls.__list_modules__(current)
 
         return modules
+
+class Container(DynamicContainer):
+    config: Configuration = Configuration()
+
+class IoC:
+    services: Container = None
+
+    def __init__(self, sanic: Sanic, config: str):
+        if not self.__class__.services:
+            self.__class__.services = Container()
+            self.services.config.from_yaml(config)
+            sanic.ext.dependency(self)
+
+            for service_name, service_definition in self.services.config().items():
+                module_path, class_name = self.__parse_service_path__(service_definition.get("class"))
+                module = import_module(module_path)
+                service_class = getattr(module, class_name)
+                provided_by = getattr(providers, service_definition["provided_by"])
+                setattr(self.services, service_name, provided_by(service_class))
+                sanic.ext.add_dependency(service_class)
+
+    @staticmethod
+    def __parse_service_path__(service_path: str = None) -> Tuple[str, str]:
+        service_path_parts = service_path.split(".")
+
+        return ".".join(service_path_parts[:-1]), service_path_parts[-1]
+
